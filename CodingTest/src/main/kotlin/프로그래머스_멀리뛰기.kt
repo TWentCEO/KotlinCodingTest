d98aaeecf0784d922a060e66d7921b06efb1d06f
TWentCEO <twent1003@gmail.com>
2023-02-06 18:11:25 +0900

    프로그래머스_멀리뛰기

---
+++ CodingTest/src/main/kotlin/프로그래머스_멀리뛰기.kt
@@ -0,0 +1,18 @@
+class Solution {
+    fun solution(n: Int): Long {
+        var answer: Long = 0
+
+        var dp = LongArray(n+1) { 0 }
+        dp[0] = 1
+        dp[1] = 1
+
+        if (n > 1) {
+            for (i in 2 .. n) {
+                dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
+            }
+        }
+
+        answer = dp[n]
+        return answer
+    }
+}
