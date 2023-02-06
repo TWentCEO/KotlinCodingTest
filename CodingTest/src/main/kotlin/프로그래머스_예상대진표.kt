09e33da76936033249005ad733b60ca2cd527a95
TWentCEO <twent1003@gmail.com>
2023-02-06 17:53:27 +0900

    프로그래머스_예상대진표

---
+++ CodingTest/src/main/kotlin/프로그래머스_예상대진표.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(n: Int, a: Int, b: Int): Int {
+        var answer = 0
+
+        var a = a
+        var b = b
+        while (a != b) {
+            a = (a + 1) / 2
+            b = (b + 1) / 2
+            answer++
+        }
+
+        return answer
+    }
+}
